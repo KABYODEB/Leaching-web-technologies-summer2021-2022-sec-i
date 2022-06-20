afd9fb140154f56c522e79debafd99d183cf1c8c
KABYO <kabyodebhridoy@gmail.com>
2022-06-20 23:15:48 +0600

    Added

--- LAB exam/UlogC.php
+++ LAB exam/UlogC.php
@@ -1,46 +1,31 @@
 <?php 
-	session_start();
+    session_start();
 
-	$userId = $_REQUEST['userId'];
-	$password = $_REQUEST['password'];
-	
-	
+    $userId = $_REQUEST['userId'];
+    $password = $_REQUEST['password'];
+    $userType=$_REQUEST['usertype'];
 
-	if ($userId== null || $password == null) {
-		echo "invalid username/password<br>";
-	}else{
-		$file = fopen('kabyo.txt', 'r');
-		while (!feof($file)) {
-				$line = fgets($file);
-				$user = explode('|', $line);
-				echo "<br>";
-				if($userId == trim($user[0]) && $password == trim($user[1] )
+   if ($userId == null || $password == null ) {
+        echo "invalid username/password/email <br>";
+    }
+    else{
+        $file = fopen('kabyo.txt', 'r');
+        while (!feof($file)) {
+                $line = fgets($file);
+                $user = explode('|', $line);
+                if($userId == trim($user[0]) && $password == trim($user[1]))
                 {
 
-					setcookie('status', 'true', time()+3600, '/');
-					if(trim($user[4]) == "user")
-
-                    {
-
-                        header('location: UserHome.php');  
-
+                    setcookie('status', 'true', time()+3600, '/');
+                    if(trim($user[4]) == "user"){
+                        header('location: UHomePage.php');	
+                    }else{
+                        header('location:AdminH.php');
                     }
-
-                    else if(trim($user[4]) == "admin")
-
-                    {
-
-                        header('location: AdminH.php');  
-
-                    }
-
                 }
-
-               
-
-            }
-		echo "invalid user!";
-	}
+        }
+        echo "invalid user!";
+    }
 
 
 
